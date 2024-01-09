1d5e8e022790153ae41d48f6ba0c2f2fd6ceb8ed
etctera <yilonglei790@gmail.com>
2024-01-09 11:53:27 +0800

    feat: rn Touchable component use

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import TouchableDemo from './src/TouchableDemo'
 
 const App = () => (
   <>
+    <TouchableDemo />
     {/* <TextDemo /> */}
     {/* <ViewDemo /> */}
     {/* <ButtonDemo /> */}
---
+++ src/TouchableDemo/index.tsx
@@ -0,0 +1,80 @@
+import { useState, type FC } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native'
+
+const TouchableDemo: FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableHighlight
+          style={styles.touchableStyle}
+          onPress={() => setCount(count + 1)}
+        >
+          <Text style={styles.txtStyle}>点击加1</Text>
+        </TouchableHighlight>
+
+        <TouchableOpacity
+          style={styles.touchableStyle}
+          onPress={() => setCount(count + 1)}
+        >
+          <Text style={styles.txtStyle}>点击加1</Text>
+        </TouchableOpacity>
+
+        <TouchableWithoutFeedback onPress={() => setCount(count + 1)}>
+          <View style={styles.touchableStyle}>
+            <Text style={styles.txtStyle}>点击加1</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <TouchableNativeFeedback onPress={() => setCount(count + 1)}>
+          <View style={styles.touchableStyle}>
+            <Text style={styles.txtStyle}>点击加1</Text>
+          </View>
+        </TouchableNativeFeedback>
+
+        <Text style={[styles.countText]}>{count !== 0 ? count : null}</Text>
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  touchableStyle: {
+    width: 300,
+    height: 38,
+    borderRadius: 5,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#06C1AE',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  txtStyle: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: 18
+  },
+  countText: {
+    marginTop: 10,
+    alignSelf: 'center',
+    fontSize: 38,
+    color: '#06C1AE'
+  }
+})
+
+export default TouchableDemo
